d9c8d533a68e5505416f9a14aa2884abe3424073
marcelizzi <marcelizzi@gmail.com>
2024-12-04 14:48:14 -0300

    V0.0

---
+++ dados_login.py
@@ -0,0 +1,10 @@
+import pyautogui
+
+email = pyautogui.prompt(text="Digite seu email", title="Dadosde Login")
+senha = pyautogui.password(text='Digite sua senha', title="Dadosde Login", mask='*' )
+
+pyautogui.click(1000,200,duration=0.5)
+
+pyautogui.typewrite(email)
+pyautogui.press('enter')
+pyautogui.typewrite(senha)
