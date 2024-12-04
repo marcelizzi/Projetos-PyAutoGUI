0953d1802b9f72769efc9a11d3159eaae50ff674
marcelizzi <marcelizzi@gmail.com>
2024-12-04 17:42:30 -0300

    V0.0

--- bot_insta.py
+++ bot_insta.py
@@ -1,12 +1,31 @@
 import pyautogui
 import pyperclip
+import webbrowser
 from time import sleep
 
-email = pyautogui.prompt(text="Digite seu email", title="Dadosde Login")
-senha = pyautogui.password(text='Digite sua senha', title="Dadosde Login", mask='*' )
+#solicitando login e senha do usuario
+login = pyautogui.prompt(text="Digite seu telefonne, nome de usuário ou email", title="Dados de Login")
+senha = pyautogui.password(text='Digite sua senha', title="Dados de Login", mask='*' )
 
-pyautogui.click(1000,200,duration=0.5)
-
-pyautogui.typewrite(email)
-pyautogui.press('enter')
+login = "easy.3dprint"
+senha = "F9ufv%%Iinsta"
+sleep(3)
+webbrowser.open('https://www.instagram.com/', new=1, autoraise=True)
+sleep(3)
+pyautogui.press('tab')
+sleep(1)
+pyautogui.press('tab')
+sleep(1)
+pyautogui.typewrite(login)
+sleep(1)
+pyautogui.press('tab')
 pyautogui.typewrite(senha)
+sleep(1)
+pyautogui.click(pyautogui.locateCenterOnScreen('login.PNG'), duration=1)
+sleep(5)
+webbrowser.open('https://www.instagram.com/nike', new=1, autoraise=True)
+sleep(5)
+pyautogui.click(485,695,duration=1)
+
+position = pyautogui.locateCenterOnScreen('emptyheart.PNG')
+print(position)
