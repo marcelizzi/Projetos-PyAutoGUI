9f84776fc35d19f71730f5753d1e9b2ded4eac29
marcelizzi <marcelizzi@gmail.com>
2024-12-04 18:07:56 -0300

    V0.1

--- bot_insta.py
+++ bot_insta.py
@@ -7,9 +7,6 @@ from time import sleep
 login = pyautogui.prompt(text="Digite seu telefonne, nome de usuário ou email", title="Dados de Login")
 senha = pyautogui.password(text='Digite sua senha', title="Dados de Login", mask='*' )
 
-login = "easy.3dprint"
-senha = "F9ufv%%Iinsta"
-sleep(3)
 webbrowser.open('https://www.instagram.com/', new=1, autoraise=True)
 sleep(3)
 pyautogui.press('tab')
@@ -25,7 +22,7 @@ pyautogui.click(pyautogui.locateCenterOnScreen('login.PNG'), duration=1)
 sleep(5)
 webbrowser.open('https://www.instagram.com/nike', new=1, autoraise=True)
 sleep(5)
-pyautogui.click(485,695,duration=1)
+pyautogui.click(940,620,duration=1)
 
 position = pyautogui.locateCenterOnScreen('emptyheart.PNG')
 print(position)
